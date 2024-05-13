a24c76ea257c659c4e7d9631f00ac15810137be7
mvksastry <mvksastry@gmail.com>
2024-05-13 20:23:36 +0530

    new login file changed

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -17,7 +17,7 @@ class AuthenticatedSessionController extends Controller
      */
     public function create(): View
     {
-        return view('auth.loginN');
+        return view('auth.login');
     }
 
     /**
