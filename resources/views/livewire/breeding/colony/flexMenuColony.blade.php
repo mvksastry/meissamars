09492f8258c3e38037af2a084253bf14d6710a55
mvksastry <mvksastry@gmail.com>
2024-05-26 08:49:49 +0530

    tweaking of presentations

--- resources/views/livewire/breeding/colony/flexMenuColony.blade.php
+++ resources/views/livewire/breeding/colony/flexMenuColony.blade.php
@@ -1,13 +1,67 @@
   <!-- Small boxes (Stat box) -->
   <div class="row">
-    
+
     <!-- .col -->
-    <div class="col-lg-3 col-6">
+    <div class="col-lg-2 col-6">
+      <!-- small box -->
+      <div class="small-box bg-success">
+        <div class="inner">
+          <h5>COLONY</h5>
+          <p>Home</p>
+        </div>
+        <div class="icon">
+          <i class="ion ion-bag"></i>
+        </div>
+        <a href="/colony-home" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+      </div>
+    </div>
+    <!-- ./col -->
+    
+
+   
+    <!-- .col -->
+    <div class="col-lg-2 col-6">
       <!-- small box -->
       <div class="small-box bg-info">
         <div class="inner">
-          <h4>EDIT ENTRY</h4>
+          <h5>ADD MATING</h5>
           <p>Cages</p>
+        </div>
+        <div class="icon">
+          <i class="ion ion-bag"></i>
+        </div>
+        <a href="/add-mating" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+      </div>
+    </div>
+    <!-- ./col -->
+
+    <!-- .col -->
+    <div class="col-lg-2 col-6">
+      <!-- small box -->
+      <div class="small-box bg-info">
+        <div class="inner">
+          <h5>ADD LITTER</h5>
+          <p>Cages</p>
+        </div>
+        <div class="icon">
+          <i class="ion ion-bag"></i>
+        </div>
+        <a href="/add-litter" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+      </div>
+    </div>
+    <!-- ./col -->
+    
+    
+    
+    
+    
+    <!-- .col -->
+    <div class="col-lg-2 col-6">
+      <!-- small box -->
+      <div class="small-box bg-warning">
+        <div class="inner">
+          <h5>EDIT</h5>
+          <p>Entry</p>
         </div>
         <div class="icon">
           <i class="ion ion-bag"></i>
@@ -15,15 +69,15 @@
         <a href="/edit-entry" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
       </div>
     </div>
-    <!-- ./col -->
-   
+    <!-- ./col -->    
+
     <!-- .col -->
-    <div class="col-lg-3 col-6">
+    <div class="col-lg-2 col-6">
       <!-- small box -->
-      <div class="small-box bg-info">
+      <div class="small-box bg-warning">
         <div class="inner">
-          <h4>EDIT MATING</h4>
-          <p>Cages</p>
+          <h5>EDIT</h5>
+          <p>Mating</p>
         </div>
         <div class="icon">
           <i class="ion ion-bag"></i>
@@ -34,33 +88,17 @@
     <!-- ./col -->
 
     <!-- .col -->
-    <div class="col-lg-3 col-6">
+    <div class="col-lg-2 col-6">
       <!-- small box -->
-      <div class="small-box bg-info">
+      <div class="small-box bg-warning">
         <div class="inner">
-          <h4>RESHUFFLE</h4>
-          <p>Cages</p>
+          <h5>EDIT</h5>
+          <p>Litter</p>
         </div>
         <div class="icon">
           <i class="ion ion-bag"></i>
         </div>
-        <a href="/reorganize" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-      </div>
-    </div>
-    <!-- ./col -->
-
-    <!-- .col -->
-    <div class="col-lg-3 col-6">
-      <!-- small box -->
-      <div class="small-box bg-info">
-        <div class="inner">
-          <h4>RESHUFFLE</h4>
-          <p>Cages</p>
-        </div>
-        <div class="icon">
-          <i class="ion ion-bag"></i>
-        </div>
-        <a href="/reorganize" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+        <a href="/edit-litter" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
       </div>
     </div>
     <!-- ./col -->
