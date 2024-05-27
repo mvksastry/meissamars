275810e14a13a81e23c6fe81d27bd08a57bc71c2
mvksastry <mvksastry@gmail.com>
2024-05-27 05:40:34 +0530

    bootstrap installation intopackage, not loading well in edge browser

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -52,3 +52,5 @@
   <link rel="stylesheet" href="{{asset('assets/plugins/sweetalert2-theme-bootstrap-4/bootstrap-4.min.css')}}">
   <!-- Toastr -->
   <link rel="stylesheet" href="{{asset('assets/plugins/toastr/toastr.min.css')}}">
+  
+  @vite(['resources/sass/app.scss', 'resources/js/app.js'])
