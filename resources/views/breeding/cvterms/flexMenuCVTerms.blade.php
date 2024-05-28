017a2422e11a7c7d5463ca7c02e54bf347561a2f
mvksastry <mvksastry@gmail.com>
2024-05-28 07:27:29 +0530

    tweaking breeding presentation

--- resources/views/breeding/cvterms/flexMenuCVTerms.blade.php
+++ resources/views/breeding/cvterms/flexMenuCVTerms.blade.php
@@ -2,37 +2,237 @@
   <div class="row">
     
     <!-- .col -->
-    
-    <div class="card-body row">
-      <div class="col-md-3">
-        <button type="button" class="btn btn-outline-primary btn-block"><i class="fa fa-bell"></i> GENE</button>
-        <button type="button" class="btn btn-outline-primary btn-block"><i class="fa fa-book"></i> GENE CLASS/button>
-        <button type="button" class="btn btn-outline-primary btn-block"><i class="fa fa-book"></i> GEN.SPEC. LOC TERMS</button>
-        <button type="button" class="btn btn-outline-primary btn-block"><i class="fa fa-book"></i> STRAIN TYPE TERMS</button>
-      </div>
-      <div class="col-md-3">
-        <button type="button" class="btn btn-outline-primary btn-block"><i class="fa fa-bell"></i> APP MATING STRAINS</button>
-        <button type="button" class="btn btn-outline-primary btn-block"><i class="fa fa-book"></i> CAUSE OF EXIT TERMS</button>
-        <button type="button" class="btn btn-outline-primary btn-block"><i class="fa fa-book"></i> COAT COLOR TERMS</button>
-        <button type="button" class="btn btn-outline-primary btn-block"><i class="fa fa-bell"></i> DIET TERMS</button>
-      </div>
-
-
-      
-      <div class="col-md-3">
-        <button type="button" class="btn btn-outline-primary btn-block"><i class="fa fa-bell"></i> SPECIES ORIGIN</button>
-        <button type="button" class="btn btn-outline-primary btn-block"><i class="fa fa-book"></i> SPECIES USE</button>
-        <button type="button" class="btn btn-outline-primary btn-block"><i class="fa fa-book"></i> BREEDING STRAINS</button>
-        <button type="button" class="btn btn-outline-primary btn-block"><i class="fa fa-book"></i> GENERATION TERMS</button>
-      </div>
-      <div class="col-md-3">
-        <button type="button" class="btn btn-outline-primary btn-block"><i class="fa fa-book"></i> LIFE STATUS</button>
-        <button type="button" class="btn btn-outline-primary btn-block"><i class="fa fa-bell"></i> STRAIN STATUS</button>
-        <button type="button" class="btn btn-outline-primary btn-block"><i class="fa fa-book"></i> USE SCHEDULE SET-UP</button>
-        <button type="button" class="btn btn-outline-primary btn-block"><i class="fa fa-book"></i> PHENOTYPE TERMS</button>
+    <div class="col-lg-3 col-6">
+      <!-- small box -->
+      <div class="small-box">
+        <a href="{{ route('strains.index') }}" class="small-box-footer"> 
+        <button type="button" class="btn btn-outline-primary btn-block">
+          <i class="fas fa-arrow-circle-right"></i>
+          GENE
+        </button>
+        </a>
       </div>
     </div>
+
+    <div class="col-lg-3 col-6">
+      <!-- small box -->
+      <div class="small-box">
+        <a href="{{ route('strains.index') }}" class="small-box-footer"> 
+        <button type="button" class="btn btn-outline-primary btn-block">
+          <i class="fas fa-arrow-circle-right"></i>
+          GENE CLASS
+        </button>
+        </a>
+      </div>
+    </div>
+    
+    
+    <div class="col-lg-3 col-6">
+      <!-- small box -->
+      <div class="small-box">
+        <a href="{{ route('strains.index') }}" class="small-box-footer"> 
+        <button type="button" class="btn btn-outline-primary btn-block">
+        <i class="fas fa-arrow-circle-right"></i>
+          GEN.SPEC. LOC TERMS
+        </button>
+        </a>
+      </div>
+    </div>
+    
+    <div class="col-lg-3 col-6">
+      <!-- small box -->
+      <div class="small-box">
+        <a href="{{ route('strains.index') }}" class="small-box-footer"> 
+        <button type="button" class="btn btn-outline-primary btn-block">
+        <i class="fas fa-arrow-circle-right"></i>
+          STRAIN TYPE TERMS
+        </button>
+        </a>
+      </div>
+    </div>
+    
+  </div>
+
+  <div class="row">  
+
+    <!-- .col -->
+    <div class="col-lg-3 col-6">
+      <!-- small box -->
+      <div class="small-box">
+        <a href="{{ route('strains.index') }}" class="small-box-footer"> 
+        <button type="button" class="btn btn-outline-primary btn-block">
+          <i class="fas fa-arrow-circle-right"></i>
+          APP MATING STRAINS
+        </button>
+        </a>
+      </div>
+    </div>
+
+    <div class="col-lg-3 col-6">
+      <!-- small box -->
+      <div class="small-box">
+        <a href="{{ route('strains.index') }}" class="small-box-footer"> 
+        <button type="button" class="btn btn-outline-primary btn-block">
+          <i class="fas fa-arrow-circle-right"></i>
+          CAUSE OF EXIT TERMS
+        </button>
+        </a>
+      </div>
+    </div>
+    
+    
+    <div class="col-lg-3 col-6">
+      <!-- small box -->
+      <div class="small-box">
+        <a href="{{ route('strains.index') }}" class="small-box-footer"> 
+        <button type="button" class="btn btn-outline-primary btn-block">
+        <i class="fas fa-arrow-circle-right"></i>
+          COAT COLOR TERMS
+        </button>
+        </a>
+      </div>
+    </div>
+    
+    <div class="col-lg-3 col-6">
+      <!-- small box -->
+      <div class="small-box">
+        <a href="{{ route('strains.index') }}" class="small-box-footer"> 
+        <button type="button" class="btn btn-outline-primary btn-block">
+        <i class="fas fa-arrow-circle-right"></i>
+          DIET TERMS
+        </button>
+        </a>
+      </div>
+    </div>  
+
+  </div>
+  
+
+
+
+
+
+  <div class="row">  
+
+    <!-- .col -->
+    <div class="col-lg-3 col-6">
+      <!-- small box -->
+      <div class="small-box">
+        <a href="{{ route('strains.index') }}" class="small-box-footer"> 
+        <button type="button" class="btn btn-outline-primary btn-block">
+          <i class="fas fa-arrow-circle-right"></i>
+          SPECIES ORIGIN
+        </button>
+        </a>
+      </div>
+    </div>
+
+    <div class="col-lg-3 col-6">
+      <!-- small box -->
+      <div class="small-box">
+        <a href="{{ route('strains.index') }}" class="small-box-footer"> 
+        <button type="button" class="btn btn-outline-primary btn-block">
+          <i class="fas fa-arrow-circle-right"></i>
+          SPECIES USE
+        </button>
+        </a>
+      </div>
+    </div>
+    
+    
+    <div class="col-lg-3 col-6">
+      <!-- small box -->
+      <div class="small-box">
+        <a href="{{ route('strains.index') }}" class="small-box-footer"> 
+        <button type="button" class="btn btn-outline-primary btn-block">
+        <i class="fas fa-arrow-circle-right"></i>
+          BREEDING STRAINS
+        </button>
+        </a>
+      </div>
+    </div>
+    
+    <div class="col-lg-3 col-6">
+      <!-- small box -->
+      <div class="small-box">
+        <a href="{{ route('strains.index') }}" class="small-box-footer"> 
+        <button type="button" class="btn btn-outline-primary btn-block">
+        <i class="fas fa-arrow-circle-right"></i>
+          GENERATION TERMS
+        </button>
+        </a>
+      </div>
+    </div>  
+
+  </div>
+
+
+
+
+
+
+
+
+
+  <div class="row">  
+
+    <!-- .col -->
+    <div class="col-lg-3 col-6">
+      <!-- small box -->
+      <div class="small-box">
+        <a href="{{ route('strains.index') }}" class="small-box-footer"> 
+        <button type="button" class="btn btn-outline-primary btn-block">
+          <i class="fas fa-arrow-circle-right"></i>
+          LIFE STATUS
+        </button>
+        </a>
+      </div>
+    </div>
+
+    <div class="col-lg-3 col-6">
+      <!-- small box -->
+      <div class="small-box">
+        <a href="{{ route('strains.index') }}" class="small-box-footer"> 
+        <button type="button" class="btn btn-outline-primary btn-block">
+          <i class="fas fa-arrow-circle-right"></i>
+          STRAIN STATUS
+        </button>
+        </a>
+      </div>
+    </div>
+    
+    
+    <div class="col-lg-3 col-6">
+      <!-- small box -->
+      <div class="small-box">
+        <a href="{{ route('strains.index') }}" class="small-box-footer"> 
+        <button type="button" class="btn btn-outline-primary btn-block">
+        <i class="fas fa-arrow-circle-right"></i>
+          USE SCHEDULE SET-UP
+        </button>
+        </a>
+      </div>
+    </div>
+    
+    <div class="col-lg-3 col-6">
+      <!-- small box -->
+      <div class="small-box">
+        <a href="{{ route('strains.index') }}" class="small-box-footer"> 
+        <button type="button" class="btn btn-outline-primary btn-block">
+        <i class="fas fa-arrow-circle-right"></i>
+          PHENOTYPE TERMS
+        </button>
+        </a>
+      </div>
+    </div>  
+
+  </div>
+
+
+
+
+ 
                
   <!-- ./col -->
-  </div> <!-- /.Small boxes end -->
+  <!-- /.Small boxes end -->
   <!-- /.row -->
